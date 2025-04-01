95aa6d80fdafb0a56d51ebe71f35eb27817aabdc
James Dawson <james.dawson@endjin.com>
2025-04-01 23:45:00 +0100

    Build-related task moved to the 'ZeroFailed.Build.Common' extension

--- module/tasks/cicd-server.properties.ps1
+++ module/tasks/cicd-server.properties.ps1
@@ -9,9 +9,3 @@ $IsGitHubActions = $false
 
 # Synopsis: When true, no DevOps agent detection will be attempted. Default is false.
 $SkipDetectCICDServer = property ZF_SKIP_DETECT_CICD_SERVER $false
-
-# Synopsis: When true, the version number will not be sent to the DevOps agent. Default is false.
-$SkipSetCICDServerBuildNumber = property ZF_SKIP_SET_CICD_SERVER_BUILDNUMBER $false
-
-# Synopsis: Defines which of the GitVersion properties is used as the build server's build number. Default is "SemVer".
-$GitVersionComponentForBuildNumber = property ZF_GITVERSION_COMPONENT_FOR_BUILDNUMBER "SemVer"

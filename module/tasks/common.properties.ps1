dd8c5e0b09133c381748e5ca297b55226228c5ba
James Dawson <james.dawson@endjin.com>
2025-07-29 15:16:26 +0100

    Refine synopsis comments

--- module/tasks/common.properties.ps1
+++ module/tasks/common.properties.ps1
@@ -13,8 +13,8 @@ $SkipEnsureGitHubCli = [Convert]::ToBoolean((property ZF_SKIP_ENSURE_GITHUB_CLI 
 # Synopsis: A hashtable of PowerShell modules to install and import. The keys are the module names and the values are hashtables with the following properties: version, repository.
 $RequiredPowerShellModules = property ZF_REQUIRED_PS_MODULES @{}
 
-# Synopsis: A collection of scriptblocks that will be run as part of InvokeBuild's 'Enter-Build' functionality
+# Synopsis: A collection of scriptblocks that will be run as part of InvokeBuild's 'Enter-Build' functionality. Extensions and processes can register their own actions by calling `$script:OnEnterActions.Add($myScriptBlock)`.
 $OnEnterActions = [List[scriptblock]]::new()
 
-# Synopsis: A collection of scriptblocks that will be run as part of InvokeBuild's 'Exit-Build' functionality, typically used as a 'finally' block for the overall process.
+# Synopsis: A collection of scriptblocks that will be run as part of InvokeBuild's 'Exit-Build' functionality, typically used as a 'finally' block for the overall process.  Extensions and processes can register their own actions by calling `$script:OnExitActions.Add($myScriptBlock)`.
 $OnExitActions = [List[scriptblock]]::new()

d7e40c405c0f4fa0cc3465d9332f60185ca42374
James Dawson <james.dawson@endjin.com>
2025-09-04 18:13:58 +0100

    Update build to include running PlatyPS

--- .zf/config.ps1
+++ .zf/config.ps1
@@ -42,7 +42,7 @@ task . FullBuild
 # task PreVersion {}
 # task PostVersion {}
 # task PreBuild {}
-# task PostBuild {}
+task PostBuild UpdateDocumentation
 # task PreTest {}
 # task PostTest {}
 # task PreTestReport {}
@@ -54,3 +54,37 @@ task . FullBuild
 # task PrePublish {}
 # task PostPublish {}
 # task RunLast {}
+
+task UpdateDocumentation {
+
+    $outPath = Join-Path $here 'docs\functions'
+    $tmpPath = Join-Path $outPath 'ZeroFailed.DevOps.Common'
+
+    # Put .md files where the New-MarkdownCommandHelp cmdlet expects to find them
+    New-Item $tmpPath -ItemType Directory -Force | Out-Null
+    Move-Item -Path $outPath\*.md -Destination $tmpPath\
+    
+    # Ensure latest version of module is imported
+    Import-Module ($PowerShellModulesToPublish[0].ModulePath) -Force
+
+    # Generate any markdown files for new functions
+    $newMarkdownCommandHelpSplat = @{
+        ModuleInfo = Get-Module ZeroFailed.DevOps.Common
+        OutputFolder = $outPath
+        HelpVersion = '1.0.0.0'
+        WithModulePage = $false
+        WarningAction = 'SilentlyContinue'  # suppress warnings about pre-existing files
+    }
+    New-MarkdownCommandHelp @newMarkdownCommandHelpSplat
+
+    # Update existing markdown files to reflect changes in latest version
+    Measure-PlatyPSMarkdown -Path $outPath\*.md |
+        Where-Object Filetype -match 'CommandHelp' |
+        Update-CommandHelp -Path {$_.FilePath} |
+        Export-MarkdownCommandHelp -OutputFolder $outPath -Force
+
+
+    # Move the markdown files back to where we want them in the repo
+    Move-Item -Path $tmpPath\*.md -Destination $outPath\
+    Remove-Item $tmpPath
+}

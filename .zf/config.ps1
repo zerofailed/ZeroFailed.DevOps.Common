d4e0e94ea05e0bb44bd13d042fe932ed737a0a44
James Dawson <james.dawson@endjin.com>
2025-09-10 15:17:46 +0100

    Test new PlatyPS feature in ZF PowerShell extension

--- .zf/config.ps1
+++ .zf/config.ps1
@@ -1,9 +1,17 @@
 # Extensions setup
 $zerofailedExtensions = @(
+    # Ensure that builds always using the current repository version of this extension:
+    # - Validates that any changes work with the build process
+    # - Avoids any conflicts when generating PlatyPS markdown documentation for the repository version
+    @{
+        Name = "ZeroFailed.DevOps.Common"
+        Path = "$here/module"
+    }
+
     @{
         Name = "ZeroFailed.Build.PowerShell"
         GitRepository = "https://github.com/zerofailed/ZeroFailed.Build.PowerShell.git"
-        GitRef = "main"
+        GitRef = "feature/add-platyps-support"
     }
     @{
         Name = "ZeroFailed.Build.GitHub"
@@ -27,6 +35,9 @@ $PowerShellModulesToPublish = @(
         AliasesToExport = @()
     }
 )
+$PSMarkdownDocsFlattenOutputPath = $true
+$PSMarkdownDocsOutputPath = './docs/functions'
+$PSMarkdownDocsIncludeModulePage = $false
 $CreateGitHubRelease = $true
 
 # Customise the build process
@@ -42,7 +53,7 @@ task . FullBuild
 # task PreVersion {}
 # task PostVersion {}
 # task PreBuild {}
-task PostBuild UpdateDocumentation
+# task PostBuild {}
 # task PreTest {}
 # task PostTest {}
 # task PreTestReport {}
@@ -54,37 +65,3 @@ task PostBuild UpdateDocumentation
 # task PrePublish {}
 # task PostPublish {}
 # task RunLast {}
-
-task UpdateDocumentation {
-
-    $outPath = Join-Path $here 'docs\functions'
-    $tmpPath = Join-Path $outPath 'ZeroFailed.DevOps.Common'
-
-    # Put .md files where the New-MarkdownCommandHelp cmdlet expects to find them
-    New-Item $tmpPath -ItemType Directory -Force | Out-Null
-    Move-Item -Path $outPath\*.md -Destination $tmpPath\
-    
-    # Ensure latest version of module is imported
-    Import-Module ($PowerShellModulesToPublish[0].ModulePath) -Force
-
-    # Generate any markdown files for new functions
-    $newMarkdownCommandHelpSplat = @{
-        ModuleInfo = Get-Module ZeroFailed.DevOps.Common
-        OutputFolder = $outPath
-        HelpVersion = '1.0.0.0'
-        WithModulePage = $false
-        WarningAction = 'SilentlyContinue'  # suppress warnings about pre-existing files
-    }
-    New-MarkdownCommandHelp @newMarkdownCommandHelpSplat
-
-    # Update existing markdown files to reflect changes in latest version
-    Measure-PlatyPSMarkdown -Path $outPath\*.md |
-        Where-Object Filetype -match 'CommandHelp' |
-        Update-CommandHelp -Path {$_.FilePath} |
-        Export-MarkdownCommandHelp -OutputFolder $outPath -Force
-
-
-    # Move the markdown files back to where we want them in the repo
-    Move-Item -Path $tmpPath\*.md -Destination $outPath\
-    Remove-Item $tmpPath
-}
